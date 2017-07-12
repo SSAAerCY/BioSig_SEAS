a33af9475bced8213c0cebe933ff4737a1240c3a
Zhuchang Zhan <zzhan@mit.edu>
2017-07-12 10:00:36 -0400

    first commit

---
+++ bin/dev/example_atmosphere_TS_simulation.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 - Massachusetts Institute of Technology (MIT)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This is an example simulation for beginner users to try out the code
+"""
+
+import os
+import sys
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(DIR, '../..'))
+
+import SEAS_Main.simulation.transmission_spectra_simulator as theory
+import SEAS_Main.simulation.observer as obs
+import SEAS_Utils.common_utils.configurable as config
+import SEAS_Utils.common_utils.data_plotter as plt
+
+
+if __name__ == "__main__":
+    
+    user_input = config.Configuration("user_input_dev.cfg")
+
+    simulation = theory.TS_Simulator(user_input)
+    
+    Raw_TS = simulation.simulate()
+    
+    
+    
+    
+    
+    
